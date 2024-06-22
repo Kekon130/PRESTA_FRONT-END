cc64566e38fdaeb3eb441e5aa6334fb2ba87b7a4
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2024-06-22 21:43:19 +0200

    Ventana de detalles del material creada y añadidas variables de entorno

---
+++ Materials/MaterialDetails/materialDetails.js
@@ -0,0 +1,101 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var userRoles = window._env_.USER_ROLES;
+
+  function checkSession() {
+    var token = getCookie('id_token');
+    if (!token) {
+      window.location.href = '../../Authentication/login.html';
+    }
+  }
+
+  checkSession();
+
+  function getQueryParams() {
+    var queryParams = {};
+    var queryString = window.location.search.slice(1);
+    var queryArray = queryString.split('&');
+
+    queryArray.forEach(function(query) {
+      var queryPair = query.split('=');
+      queryParams[decodeURIComponent(queryPair[0])] = decodeURIComponent(queryPair[1]);
+    });
+
+    return queryParams;
+  }
+
+  function getUserRol() {
+    var token = getCookie('id_token');    
+    var payload = token.split('.')[1];
+    var decodedPayload = atob(payload);
+    var payloadObj = JSON.parse(decodedPayload);
+    return payloadObj['cognito:groups'] || [];
+  }
+
+  var params = getQueryParams();
+  var materialID = params.id;
+  var materialType = params.type;
+
+  var materialDetails = document.getElementById('materialDetails');
+
+  fetch(`${window._env_.BASE_API_URL}/material/${materialType}/${materialID}`, {
+    method: 'GET',
+    headers: {
+      auth: getCookie('id_token')
+    }
+  })
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return response.json().then(function(error) {
+        throw new Error(error.message);
+      });
+    }
+  })
+  .then(function(data) {
+    var material = data.Material;
+    var materialDetailsHTML = `
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${material.Nombre}</h5>
+          <p class="card-text">Unidades Totales: ${material.Cantidad}</p>
+          <p class="card-text">Disponibles: ${material.Unidades_Disponibles}</p>
+          ${material.ISBN ? `<p class="card-text">ISBN: ${material.ISBN}</p>` : ''}
+          ${material.Año_de_Publicación ? `<p class="card-text">Año de Publicación: ${material.Año_de_Publicación}</p>` : ''}
+          ${material.Autor ? `<p class="card-text">Autor: ${material.Autor}</p>` : ''}
+          ${material.Modelo ? `<p class="card-text">Modelo: ${material.Modelo}</p>` : ''}
+          ${material.Asignatura ? `<p class="card-text">Asignatura: ${material.Asignatura}</p>` : ''}
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary" id="reserveButton">Reservar</button>
+          <button class="btn btn-warning" id="editButton" style="display: none;">Modificar</button>
+          <button class="btn btn-danger" id="deleteButton" style="display: none;">Eliminar</button>
+        </div>
+      </div>`;
+
+    materialDetails.innerHTML = materialDetailsHTML;
+
+    var userRol = getUserRol();
+    if (userRol.includes(userRoles.GESTORES)) {
+      document.getElementById('editButton').style.display = 'inline-block';
+      document.getElementById('deleteButton').style.display = 'inline-block';
+    }
+  })
+  .catch(function(error) {
+    console.log(error);
+    materialDetails.innerHTML = `<p>Error al cargar el material: ${error.message}.</p>`;
+  });
+});
+
+function getCookie(name) {
+  var cookieArr = document.cookie.split(';');
+
+  for (var i = 0; i < cookieArr.length; i++) {
+    var cookiePair = cookieArr[i].split('=');
+    if (name === cookiePair[0].trim()) {
+      return decodeURIComponent(cookiePair[1]);
+    }
+  }
+
+  return null;
+}
--- Materials/materials.js
+++ Materials/materials.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  //checkSession();
+  checkSession();
 
   var materialsList = document.getElementById('materialsList');
 
-  fetch('https://qfqzi5nng6.execute-api.eu-north-1.amazonaws.com/Second_Sprint/material', {
+  fetch(`${window._env_.BASE_API_URL}/material`, {
     method: 'GET',
     headers: {
-      'auth': 'eyJraWQiOiJvRkZ5V1o5ODE2ZnFaZklFNGh4U3M3MVRqYjJENEFzc1ZRVERyVXgyRmhZPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI3MGNjNjkwYy01MDAxLTcwMjgtNzZjYy1kZTQ5ZTNhMjczYmUiLCJjb2duaXRvOmdyb3VwcyI6WyJHZXN0b3JlcyJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LW5vcnRoLTEuYW1hem9uYXdzLmNvbVwvZXUtbm9ydGgtMV9VYThqak1BMFoiLCJjb2duaXRvOnVzZXJuYW1lIjoiNzBjYzY5MGMtNTAwMS03MDI4LTc2Y2MtZGU0OWUzYTI3M2JlIiwib3JpZ2luX2p0aSI6IjUzOGJhZjdmLTk0YTctNDg1Mi1hMTcyLWEzZGFmNDBkMzMyOCIsImF1ZCI6IjE4Ym1uczU2bjF2c2VqaTZpNmwwaGUzdXBiIiwiZXZlbnRfaWQiOiIxMWNlY2U1YS0wMGUzLTQzNTctYTFiZi03N2E3N2JlOTQ2NjMiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTcxODk4NDU3MCwibmFtZSI6IlZpY3RvcmlhIFBlbGFkbyBTw6FuY2hleiIsImV4cCI6MTcxODk4ODE3MCwiaWF0IjoxNzE4OTg0NTcwLCJqdGkiOiJlZGNlZmExZi01ZDc5LTRjYzktYmVhOC1iOGZkNTI1M2ZkZmEiLCJlbWFpbCI6InZpY3RvcmlhLnBlbGFkb0BhbHVtbm9zLnVwbS5lcyJ9.Qs3agGldt4fqoI9_thSvkRLn4ydQ_6q43nEFk0pf_PEUcIADnZW28cOpFpowx9uUud6SSpiuvHffOue8UnUBJluNO05zfXL7C38HEdNJiIdFpRk6BiSqxWm-scdJhwg5_B3SKb20ceLZpoV4c7Cw4gqPRoZ3F_QDiDIrWktjQUEoCPlcyyLbHDNOKTAX5NN39eggDKfO1qGyKyGCShk7ZiGM6Cp2Lx1eXwQ-Ni4fZQevD2Z4IEwc9rVxw0PvhOYlogs0mvANErN_kEKp07pUsXImlk4_2H0cClMl08kw-dRZm0AmD6CXvOlHhrycAPcSSvR3LXzAZXThXJHt_xOyqA'
+      'auth': getCookie('id_token')
     }
   })
   .then(function(response) {
@@ -27,11 +27,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var materialsHTML = '';
 
     Object.keys(data).forEach(function(key) {
-      materialsHTML += `<h3>${key.charAt(0).toUpperCase() + key.slice(1)}</h3>`;
+      var materialsType = key.charAt(0).toUpperCase() + key.slice(1);
+      console.log(materialsType);
+      materialsHTML += `<h3>${materialsType}</h3>`;
 
-      data[key].forEach(function(material) {
+      data[key].forEach(function(material, index) {
+        var materialID = index + 1;
         materialsHTML += `
-        <div class="card mb-3">
+        <a href="./MaterialDetails/materialDetails.html?id=${encodeURIComponent(materialID)}&type=${encodeURIComponent(materialsType)}" class="mb-3 text-decoration-none text-dark">
+          <div class="card mb-3">
             <div class="card-body">
                 <h5 class="card-title">${material.Nombre}</h5>
                 <p class="card-text">Disponibles: ${material.Unidades_Disponibles}</p>
@@ -40,7 +44,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 ${material.Modelo ? `<p class="card-text">Modelo: ${material.Modelo}</p>` : ''}
                 <p class="card-text">Asignatura: ${material.Asignatura}</p>
             </div>
-        </div>`;
+          </div>
+        </a>`;
       });
     });
     materialsList.innerHTML = materialsHTML;
